df380a30c95fe87ee257f27b8beadaa006feb3f1
manish5999 <itsyourdevil219@gmail.com>
2026-07-29 21:56:39 +0530

    Implement search in rotated sorted array

--- assignment1.java
+++ assignment1.java
@@ -70,6 +70,37 @@ return false;
 
 
 //q4)33 – Search in Rotated Sorted Array
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } 
+            else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
 //q5)162 – Find Peak Element
 class Solution {
     public int findPeakElement(int[] nums) {
